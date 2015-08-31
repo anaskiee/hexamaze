455c2ea92b5c9a2583b213055587fb347530ddc9
Eric Anaskieviez <eric.anaskieviez@gmail.com>
2015-08-31 08:16:53 +0200

    forgot 2 files

---
+++ js/events/event.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function Event() {
+	this.receiver = null;
+	this.resultReceiver = null;
+}
+
+Event.prototype.setReceiver = function(receiver) {
+	this.receiver = receiver;
+}
+
+Event.prototype.setResultReceiver = function(resReceiver) {
+	this.resultReceiver = resReceiver;
+}
+
+Event.prototype.execute = function() {
+}
+
+Event.prototype.handleResult = function(result) {
+	if (this.resultReceiver != null) {
+		this.resultReceiver.handleEventResult(result);
+	} else {
+		console.log("no handler for this event result : " + result);
+	}
+}
---
+++ js/gameMode.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function GameMode(name) {
+	this.name = name;
+}
+
+GameMode.prototype.pushMouseEvent = function(event) {
+}
+
+GameMode.prototype.pushMessageEvent = function(event) {
+}
+
+GameMode.prototype.pushKeyboardEvent = function(event) {
+}
