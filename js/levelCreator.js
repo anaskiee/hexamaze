a47f1a758df655e691441e9aea9d6cd8f4ad861b
Eric Anaskieviez <eric.anaskieviez@gmail.com>
2015-09-01 07:59:45 +0200

    add function to convert a level into an editing structure

--- js/levelCreator.js
+++ js/levelCreator.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function LevelCreator(level, nbLines, nbColumns) {
+	this.hexagons = null;
 	this.level = level;
 	this.nbLines = nbLines + 2;
 	this.nbColumns = nbColumns + 2;
@@ -32,12 +33,20 @@ LevelCreator.prototype.createBasicLevel = function() {
 		}
 	}
 
+	this.setLinks();
+}
+
+LevelCreator.prototype.setLinks = function() {
 	var hexagon;
 	// Initialize links between hexagons
 	for (var i = 0; i < this.nbLines; i++) {
 		for (var j = 0; j < this.nbColumns; j++) {
 			var idx = i*this.nbColumns + j;
 			hexagon = this.hexagons[i][j];
+
+			if (hexagon == null) {
+				continue;
+			}
 
 			// Links
 			// top
@@ -127,4 +136,100 @@ LevelCreator.prototype.getRandomHexagon = function() {
 	var x = Math.floor(Math.random() * (this.nbColumns - 2)) + 1;
 	var y = Math.floor(Math.random() * (this.nbLines - 2)) + 1;
 	return this.hexagons[y][x];
+}
+
+LevelCreator.prototype.fillEditingStructure = function() {
+	var currHex, nextHex;
+	var directions = ["top", "topLeft", "topRight", "bot", "botLeft", "botRight"];
+	var hexagons = [];
+	var marks = new Map();
+	hexagons.push(this.level.exitHexagon);
+	marks.set(this.level.exitHexagon, [0, 0]);
+	var position, i, j;
+
+	// Compute all relative positions
+	while (hexagons.length > 0) {
+		currHex = hexagons.shift();
+		position = marks.get(currHex);
+		i = position[0];
+		j = position[1];
+		for (var dir of directions) {
+			nextHex = currHex[dir];
+			if (nextHex !== null && !marks.has(nextHex)) {
+				hexagons.push(nextHex);
+				switch (dir) {
+					case "top":
+						marks.set(nextHex, [i - 1, j]);
+						break;
+					case "bot":
+						marks.set(nextHex, [i + 1, j]);
+						break;
+					case "topLeft":
+						if (j % 2 == 0) {
+							marks.set(nextHex, [i - 1, j - 1]);
+						} else {
+							marks.set(nextHex, [i, j - 1]);
+						}
+						break;
+					case "topRight":
+						if (j % 2 == 0) {
+							marks.set(nextHex, [i - 1, j + 1]);
+						} else {
+							marks.set(nextHex, [i, j + 1]);
+						}
+						break;
+					case "botLeft":
+						if (j % 2 == 0) {
+							marks.set(nextHex, [i, j - 1]);
+						} else {
+							marks.set(nextHex, [i + 1, j - 1]);
+						}
+						break;
+					case "botRight":
+						if (j % 2 == 0) {
+							marks.set(nextHex, [i, j + 1]);
+						} else {
+							marks.set(nextHex, [i + 1, j + 1]);
+						}
+						break;
+				}
+			}
+		}
+	}
+
+	// Compute max dimensions
+	var iMin = 0
+	var iMax = 0
+	var jMin = 0
+	var jMax = 0;
+	for (var relPos of marks.values()) {
+		iMin = Math.min(iMin, relPos[0]);
+		iMax = Math.max(iMax, relPos[0]);
+		jMin = Math.min(jMin, relPos[1]);
+		jMax = Math.max(jMax, relPos[1]);
+	}
+
+	// Init reference structure
+	this.nbLines = iMax - iMin + 1;
+	this.nbColumns = jMax - jMin + 1;
+	this.hexagons = new Array(this.nbLines);
+	for (var i = 0; i < this.nbLines; i++) {
+		this.hexagons[i] = new Array(this.nbColumns);
+		for (var j = 0; j < this.nbColumns; j++) {
+			this.hexagons[i][j] = null;
+		}
+	}
+
+	for (var mark of marks.values()) {
+		mark[0] -= iMin;
+		mark[1] -= jMax;
+	}
+
+	// Place hexagons in array
+	for (var [hex, pos] of marks) {
+		this.hexagons[pos[0]][pos[1]] = hex;
+	}
+
+	// Init links
+	this.setLinks();
 }
