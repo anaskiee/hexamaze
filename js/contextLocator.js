51a5b676188d69a5444182f9cbf76662aa857710
Eric Anaskieviez <eric.anaskieviez@gmail.com>
2019-05-12 21:30:39 +0200

    Commit forgotten file contextLocator.js

---
+++ js/contextLocator.js
@@ -0,0 +1,7 @@
+// Little locator to be able to update them dynamically
+// and also to lighten object creation
+function ContextLocator(ctx, offCtx)
+{
+    this.ctx = ctx;
+    this.offCtx = offCtx;
+}
